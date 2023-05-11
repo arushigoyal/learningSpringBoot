12251f1a17fb63fc61f0f7cf4961cba214a0efab
arushi <arushi.goyal@hotmail.com>
2023-05-11 17:57:49 +0530

    swagger integrated in Spring boot app

--- src/main/java/com/example/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/example/springbootexample/SpringBootExampleApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootExampleApplication {
 
     public static void main(String[] args) {
